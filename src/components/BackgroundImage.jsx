01a42bd8f2520c7db21017009bd70c85d8de841a
derek-williams00 <derekOfficial00@gmail.com>
2023-10-03 11:43:43 -0700

    added content

--- src/components/BackgroundImage.jsx
+++ src/components/BackgroundImage.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-// <img src={src} className="background-image" alt="background" />
 
 const BackgroundImage = ({ src }) => {
   return (
     <>
-        
         <div className="parallax" style={{ backgroundImage: `url(${src})` }}></div>
     </>
   );
--- src/index.jsx
+++ src/index.jsx
@@ -20,26 +20,23 @@ const App = () => {
     <>
       <NavBar />
       <Bio />
-      <BackgroundImage
-        src="./images/robotics_trophy.jpg"
-      />
+      <BackgroundImage src="./images/robotics_trophy.jpg" />
       <Grid
         id="completed"
         title="Completed Projects"
         items={completed}
       />
+      <BackgroundImage src="./images/ocilloscope.jpg" />
       <Grid
         id="wip"
         title="Works in Progress"
         items={wip}
       />
+      <BackgroundImage src="./images/groundstation.jpg" />
       <Grid 
         id="experience"
         title="Experiences"
         items={experiences}
-      />
-      <BackgroundImage
-        src="./images/groundstation.jpg"
       />
       <Footer />
     </>
