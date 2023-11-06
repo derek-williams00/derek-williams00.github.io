fcd6a8c1f85343d840000c94d6e15a4af9c6be28
derek-williams00 <derekOfficial00@gmail.com>
2023-11-05 21:04:01 -0800

    adding images to webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path');
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
 
 module.exports = {
   entry: './src/index.jsx',
@@ -11,7 +14,12 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
-    })
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'public/images', to: 'images' }
+      ],
+    }),
   ],
   module: {
     rules: [
